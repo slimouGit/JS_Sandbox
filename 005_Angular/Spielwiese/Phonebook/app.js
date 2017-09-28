aca9b6576e3ec6a324adba356abe1e7b0da2fe62
Salim Oussayfi <box@slimou.net>
2017-09-28 07:22:06 +0200

    ...

--- 005_Angular/Spielwiese/Phonebook/app.js
+++ 005_Angular/Spielwiese/Phonebook/app.js
@@ -5,5 +5,8 @@ angular.module('contactApp', [])
     .controller('ContactsCtrl', function($scope, $http){
         $http.get('contacts.json').then(function(contactsResponse) {
             $scope.contacts = contactsResponse.data;
+            console.log(JSON.stringify($scope.contacts));
+            //$scope.contactData = JSON.stringify($scope.contactData);
+            $scope.contactData = "Test";
         });
     });
