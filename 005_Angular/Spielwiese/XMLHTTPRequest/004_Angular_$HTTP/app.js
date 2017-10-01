aad384386a26e46541cf954ac0bd3800ab7082a1
Salim Oussayfi <box@slimou.net>
2017-10-01 12:00:10 +0200

    ...

--- 005_Angular/Spielwiese/XMLHTTPRequest/004_Angular_$HTTP/app.js
+++ 005_Angular/Spielwiese/XMLHTTPRequest/004_Angular_$HTTP/app.js
@@ -5,14 +5,14 @@ app.controller("TutorialController", function($scope, $http) {
     $scope.cities = [];
 
     $scope.$watch("showCities", function() {
-        if ($scope.showCities == true) {
+        //if ($scope.showCities == true) {
             $http.get("cities.json").then(function(data){
                 console.log(data);
                 $scope.cities = data.data;
             }, function(error){
                 alert("Ferhler");
             });
-        }
+        //}
     });
 
 });
