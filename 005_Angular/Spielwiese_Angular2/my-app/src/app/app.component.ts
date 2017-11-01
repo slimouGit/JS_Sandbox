8631b440895e6429c40b70c5fe72c0e044676a26
Salim Oussayfi <box@slimou.net>
2017-11-01 19:59:38 +0100

    ...

---
+++ 005_Angular/Spielwiese_Angular2/my-app/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
+
+class Car{
+  id: number;
+  brand: string;
+  name: string;
+}
+/**
+car : Car = {
+  id: 1,
+  brand: "BMW",
+  name: "5er"
+};
+**/
